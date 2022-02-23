8878b2d20f046ad593191d09c94f86a38f3916f6
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-02-23 15:29:30 +0000

    Add getGenomeAttribute function

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -135,4 +135,17 @@ class WorkflowMain {
         }
         return is_synapse
     }
+
+    //
+    // Get attribute from genome config file e.g. fasta
+    //
+    public static String getGenomeAttribute(params, attribute) {
+        def val = ''
+        if (params.genomes && params.genome && params.genomes.containsKey(params.genome)) {
+            if (params.genomes[ params.genome ].containsKey(attribute)) {
+                val = params.genomes[ params.genome ][ attribute ]
+            }
+        }
+        return val
+    }
 }
